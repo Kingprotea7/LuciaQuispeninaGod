12edd4148945e2b2842e7491ca7e8c3e2521ea7e
Fabian Alfaro <Ranshesi7@gmail.com>
2023-06-22 20:35:37 -0500

    Agregar archivos de proyecto.

---
+++ LuciaQuispeninaGod/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using LuciaQuispeninaGod.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LuciaQuispeninaGod.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<User> GetUsers()
+        {
+            return new List<User>
+            {
+                new User { Id = 1, Name = "Jorge" },
+                new User { Id = 2, Name = "Jorge2" },
+                new User { Id = 3, Name = "Jorge3" }
+
+            };
+        }
+    }
+}
---
+++ LuciaQuispeninaGod/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace LuciaQuispeninaGod.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+          
+    }
+}
